1feb5214c3506eaa05c14d818ef09e179defb3cb
Juliasmatius <80146546+Juliasmatius@users.noreply.github.com>
2022-12-23 18:04:11 +0200

    Update update.py

--- update.py
+++ update.py
@@ -1,4 +1,6 @@
 import requests
+import winshell
+from win32com.client import Dispatch
 url = 'https://raw.githubusercontent.com/Juliasmatius/fjjfgjhfg/main/main.py'
 r = requests.get(url, allow_redirects=True)
 open('c:/julinjutut/main.py', 'wb').write(r.content)
