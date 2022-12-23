8ef1142db30d84868fec51463e57169bd11cf128
Juliasmatius <80146546+Juliasmatius@users.noreply.github.com>
2022-12-23 09:54:23 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import os
+import winshell
+from win32com.client import Dispatch
 def update():
 	print("Updating...")
-	os.system('start "Updating... " python3 c:/julinjutut/update.py')
+	os.system('python3 c:/julinjutut/update.py')
+def createshortcut():
+	desktop = winshell.desktop()
+	path = os.path.join(desktop, "Julin jutut.lnk")
+	target = r"c:/julinjutut/main.py"
+	wDir = r"c:/julinjutut"
+	shell = Dispatch('WScript.Shell')
+	shortcut = shell.CreateShortCut(path)
+	shortcut.Targetpath = target
+	shortcut.WorkingDirectory = wDir
+	shortcut.save()	
 def start():
 	print("Welcome")
 	print("1. Update")
-	print("2. Exit")
+	print("2. Create a shortcut to desktop")
+	print("3. Exit")
 	selection = input("Selection : ")
 	if selection == "1":
 		update()
 	else:
-		if selection == "2":
-			exit()
+		if selection == "2"
+			createshortcut()
 		else:
-			print("Invalid selection")
-			start()
+			if selection == "3":
+				exit()
+			else:
+				print("Invalid selection")
+				start()
 start()
