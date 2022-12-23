f7beacaf761a4a8b96aa39f3973a44181f4fc64e
Juliasmatius <80146546+Juliasmatius@users.noreply.github.com>
2022-12-23 17:59:51 +0200

    Update install.py

--- install.py
+++ install.py
@@ -8,6 +8,10 @@ def install():
 	url = 'https://raw.githubusercontent.com/Juliasmatius/fjjfgjhfg/main/update.py'
 	r = requests.get(url, allow_redirects=True)
 	open('c:/julinjutut/update.py', 'wb').write(r.content)
+	url = 'https://raw.githubusercontent.com/Juliasmatius/fjjfgjhfg/main/autorun.py'
+	r = requests.get(url, allow_redirects=True)
+	open('c:/julinjutut/autorun.py', 'wb').write(r.content)
+	REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "Julinjutut" /t REG_SZ /F /D "C:\julinjutut\autorun.bat"
 	os.system("python3 c:/julinjutut/main.py")
 def start():
 	shure = input("Are you shure you want to install? [Y/N] ")
