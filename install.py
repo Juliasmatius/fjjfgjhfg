47272df3c2c494caf58302a00d2999654b5afa74
Juliasmatius <80146546+Juliasmatius@users.noreply.github.com>
2022-12-23 17:52:01 +0200

    Add files via upload

--- install.py
+++ install.py
@@ -1,10 +1,21 @@
-
+import os
+import requests
+def install():
+	os.mkdir("c:/julinjutut")
+	url = 'https://raw.githubusercontent.com/Juliasmatius/fjjfgjhfg/main/main.py'
+	r = requests.get(url, allow_redirects=True)
+	open('c:/julinjutut/main.py', 'wb').write(r.content)
+	url = 'https://raw.githubusercontent.com/Juliasmatius/fjjfgjhfg/main/update.py'
+	r = requests.get(url, allow_redirects=True)
+	open('c:/julinjutut/update.py', 'wb').write(r.content)
+	os.system("python3 c:/julinjutut/main.py")
 def start():
-	shure = input("Are you shure you want to install? [Y/N]")
+	shure = input("Are you shure you want to install? [Y/N] ")
 	if shure == "N":
 		exit()
 	if shure == "Y":
-		print("OK")
+		print("Installing")
+		install()
 	print("Invalid")
 	start()
 start()
