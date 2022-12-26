e75578567762383efc4c170d1b8ab83984566947
Juliasmatius <80146546+Juliasmatius@users.noreply.github.com>
2022-12-26 14:08:43 +0200

    Made it download the roblox anti afk script

--- update.py
+++ update.py
@@ -7,5 +7,8 @@ open('c:/julinjutut/main.py', 'wb').write(r.content)
 url = 'https://raw.githubusercontent.com/Juliasmatius/fjjfgjhfg/main/autorun.py'
 r = requests.get(url, allow_redirects=True)
 open('c:/julinjutut/autorun.py', 'wb').write(r.content)
+url = 'https://raw.githubusercontent.com/Juliasmatius/fjjfgjhfg/main/roblox_antiafk.py'
+r = requests.get(url, allow_redirects=True)
+open('c:/julinjutut/roblox_antiafk.py', 'wb').write(r.content)
 import os
 os.system("python3 c:/julinjutut/main.py")
