8fc9b0346308a2a04dbadade770170b23bba88d9
Juliasmatius <80146546+Juliasmatius@users.noreply.github.com>
2022-12-26 14:13:18 +0200

    My code was dumb so i removed most of it

--- install.py
+++ install.py
@@ -8,9 +8,6 @@ def install():
 	r = requests.get(url, allow_redirects=True)
 	open('c:/julinjutut/sudo.zip', 'wb').write(r.content)
 	tar -xf sudo.zip
-	url = 'https://raw.githubusercontent.com/Juliasmatius/fjjfgjhfg/main/main.py'
-	r = requests.get(url, allow_redirects=True)
-	open('c:/julinjutut/main.py', 'wb').write(r.content)
 	url = 'https://raw.githubusercontent.com/Juliasmatius/fjjfgjhfg/main/update.py'
 	r = requests.get(url, allow_redirects=True)
 	open('c:/julinjutut/update.py', 'wb').write(r.content)
@@ -18,7 +15,7 @@ def install():
 	r = requests.get(url, allow_redirects=True)
 	open('c:/julinjutut/autorun.py', 'wb').write(r.content)
 	c:/julinjutut/x86/gsudo.exe REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "Julinjutut" /t REG_SZ /F /D "C:\julinjutut\autorun.bat"
-	os.system("python3 c:/julinjutut/main.py")
+	os.system("python3 c:/julinjutut/update.py")
 def start():
 	shure = input("Are you shure you want to install? [Y/N] ")
 	if shure == "N":
