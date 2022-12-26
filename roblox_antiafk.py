36bae9c8c8a5bf189842e20ec49ea93a0e3b71f7
Juliasmatius <80146546+Juliasmatius@users.noreply.github.com>
2022-12-26 14:07:17 +0200

    Added Roblox Anti-AFK script

---
+++ roblox_antiafk.py
@@ -0,0 +1,30 @@
+import keyboard
+import time
+import os
+direction = 1
+os.system("timeout 10")
+totaltimes = 0
+while True:
+	times = 20
+	if direction == 0:
+		direction = 1
+		while times > 0:
+			keyboard.press_and_release('w')
+			time.sleep(0.5)
+			times = times - 1
+			print(times)
+			totaltimes = totaltimes + 1
+		direction = 1
+	if direction == 1:
+			direction = 0
+			while times > 0:
+				keyboard.press_and_release('s')
+				time.sleep(0.5)
+				times = times - 1
+				print(times)
+				totaltimes = totaltimes + 1
+			direction = 0
+	print("direction")
+	print(direction)
+	print("Total times")
+	print(totaltimes)
