e2da308d7078df83a8ac7c904607ae368824d6ce
Juliasmatius <80146546+Juliasmatius@users.noreply.github.com>
2023-01-01 17:12:19 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,23 @@
+import time
+import random
 import os
-import winshell
-from win32com.client import Dispatch
-def roblox_antiafk():
-	os.system("python3 c:/julinjutut/roblox_antiafk.py")
-def update():
-	print("Updating...")
-	os.system('python3 c:/julinjutut/update.py')
-def createshortcut():
-	desktop = winshell.desktop()
-	path = os.path.join(desktop, "Julin jutut.lnk")
-	target = r"c:/julinjutut/update.py"
-	wDir = r"c:/julinjutut"
-	shell = Dispatch('WScript.Shell')
-	shortcut = shell.CreateShortCut(path)
-	shortcut.Targetpath = target
-	shortcut.WorkingDirectory = wDir
-	shortcut.save()	
-	start()
-def start():
-	print("Welcome")
-	print("1. Update")
-	print("2. Create a shortcut to desktop")
-	print("3. Exit")
-	print("4. Roblox Anti-AFK")
-	selection = input("Selection : ")
-	if selection == "4":
-		roblox_antiafk()
-	if selection == "1":
-		update()
-	else:
-		if selection == "2":
-			createshortcut()
-		else:
-			if selection == "3":
-				exit()
-			else:
-				print("Invalid selection")
-				start()
-start()
+while True:
+	timewait = random.randint(300, 900)
+	print("Time seconds "+str(timewait))
+	print("Time minutes "+str(timewait / 60))
+	jsselect = round(random.randint(1, 9) / 3)
+	print("Jumpscare num : "+str(jsselect))
+	os.system("timeout /t "+str(timewait)+"")
+	
+	if jsselect < 1:
+		os.system('start /d "%ProgramFiles(x86)%\Windows Media Player" wmplayer.exe "c:/jumpscarer/jump1.mp4"')
+		time.sleep(33)
+		os.system("taskkill /F /IM wmplayer.exe")
+	if jsselect == 2:
+		os.system('start /d "%ProgramFiles(x86)%\Windows Media Player" wmplayer.exe "c:/jumpscarer/jump2.mp4"')
+		time.sleep(2)
+		os.system("taskkill /F /IM wmplayer.exe")
+	if jsselect == 3:
+		os.system('start /d "%ProgramFiles(x86)%\Windows Media Player" wmplayer.exe "c:/jumpscarer/jump3.mp4"')
+		time.sleep(2)
+		os.system("taskkill /F /IM wmplayer.exe")
