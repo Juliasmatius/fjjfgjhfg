1431fcb8ea36a679680fed979ccdda184d0da38a
Juliasmatius <80146546+Juliasmatius@users.noreply.github.com>
2023-03-04 09:50:02 +0200

    Update comms_install.bat

--- comms_install.bat
+++ comms_install.bat
@@ -1,3 +1,4 @@
 @echo off
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/Juliasmatius/fjjfgjhfg/main/http_communication.zip -OutFile package.zip"
 powershell -command "Expand-Archive package.zip the_thing"
+del package.zip
